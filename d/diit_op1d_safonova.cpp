64ba638dacf2aa432522fc08f7a4e597ac77f796
atwucn <dseecnin@gamil.com>
2021-09-15 15:59:37 +0300

    add while and do while loop

--- d/diit_op1d_safonova.cpp
+++ d/diit_op1d_safonova.cpp
@@ -9,7 +9,25 @@ int main() {
 	// 2. Print table
 	std::cout << "inch | cm " << std::endl;
 
+	// for
 	for (int i = 10; i <= n; i++) {
 		std::cout << i << " | " << i * 2.54 << std::endl;
 	}
+
+	// while
+	int i = 10;
+	while (i <= n) {
+		std::cout << i << " | " << i * 2.54 << std::endl;
+		i++;
+	}
+
+	// do while
+	i = 10;
+	do {
+		std::cout << i << " | " << i * 2.54 << std::endl;
+		i++;
+
+	} while (i <= n);
+
+	system("pause");
 }
